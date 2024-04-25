39cfc6db0b0617f48eaf286f830a882b1c5f15e6
TellMin <dhgirkcls@gmail.com>
2024-04-25 21:24:03 +0900

    システムプラグインのビルド方法を変更

--- src/plugins/systems.rs
+++ src/plugins/systems.rs
@@ -4,12 +4,11 @@ pub struct SystemsPlugin;
 
 impl Plugin for SystemsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
-        app.add_systems(Update, bevy::window::close_on_esc);
+        app.add_systems(Startup, setup)
+            .add_systems(Update, bevy::window::close_on_esc);
     }
 }
 
 fn setup(mut commands: Commands) {
-    // Camera
     commands.spawn(Camera2dBundle::default());
 }
