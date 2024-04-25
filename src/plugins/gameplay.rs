82a4d510a6cc900dcf5306ef11ecff8aea478d9e
TellMin <dhgirkcls@gmail.com>
2024-04-25 21:54:54 +0900

    ゲームプレイの状態を追加し、ぷよが落下するように修正

--- src/plugins/gameplay.rs
+++ src/plugins/gameplay.rs
@@ -1,37 +1,57 @@
+use crate::states::app::AppState;
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 pub struct GameplayPlugin;
 
 impl Plugin for GameplayPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_puyo)
-            .add_systems(Update, fall_puyo);
+        app.add_systems(OnEnter(AppState::Gameplay), spawn_puyo)
+            .add_systems(Update, fall_puyo.run_if(in_state(AppState::Gameplay)))
+            .add_systems(OnExit(AppState::Gameplay), remove_entity);
     }
 }
 
 #[derive(Component)]
 struct Puyo;
 
+#[derive(Resource)]
+pub struct GameData {
+    gameplay_entity: Entity,
+}
+
 fn spawn_puyo(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn((
-        MaterialMesh2dBundle {
-            mesh: meshes.add(Circle::default()).into(),
-            material: materials.add(Color::rgb(1.0, 0.5, 0.5)),
-            transform: Transform::from_translation(Vec3::new(0.0, -50.0, 1.0))
-                .with_scale(Vec2::splat(30.).extend(1.)),
-            ..default()
-        },
-        Puyo,
-    ));
+    let puyo_entity = commands
+        .spawn((
+            MaterialMesh2dBundle {
+                mesh: meshes.add(Circle::default()).into(),
+                material: materials.add(Color::rgb(1.0, 0.5, 0.5)),
+                transform: Transform::from_translation(Vec3::new(0.0, -50.0, 1.0))
+                    .with_scale(Vec2::splat(30.).extend(1.)),
+                ..default()
+            },
+            Puyo,
+        ))
+        .id();
+
+    commands.insert_resource(GameData {
+        gameplay_entity: puyo_entity,
+    });
 }
 
 // ぷよを落下させる
 fn fall_puyo(mut query: Query<&mut Transform, With<Puyo>>) {
     for mut transform in query.iter_mut() {
-        transform.translation.y -= 1.0;
+        if transform.translation.y > -200.0 {
+            transform.translation.y -= 1.0;
+        }
     }
+}
+
+// Entityを削除する
+fn remove_entity(mut commands: Commands, game_data: Res<GameData>) {
+    commands.entity(game_data.gameplay_entity).despawn();
 }
--- src/plugins/systems.rs
+++ src/plugins/systems.rs
@@ -1,3 +1,4 @@
+use crate::states::app::AppState;
 use bevy::prelude::*;
 
 pub struct SystemsPlugin;
@@ -5,7 +6,8 @@ pub struct SystemsPlugin;
 impl Plugin for SystemsPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
-            .add_systems(Update, bevy::window::close_on_esc);
+            .add_systems(Update, bevy::window::close_on_esc)
+            .init_state::<AppState>();
     }
 }
 
