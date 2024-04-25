45025de7374b9c45104a2401967967a9b319deb4
TellMin <dhgirkcls@gmail.com>
2024-04-25 21:22:54 +0900

    ぷよの落下処理を追加

--- src/plugins/gameplay.rs
+++ src/plugins/gameplay.rs
@@ -4,20 +4,34 @@ pub struct GameplayPlugin;
 
 impl Plugin for GameplayPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_puyo);
+        app.add_systems(Startup, spawn_puyo)
+            .add_systems(Update, fall_puyo);
     }
 }
+
+#[derive(Component)]
+struct Puyo;
 
 fn spawn_puyo(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(Circle::default()).into(),
-        material: materials.add(Color::rgb(1.0, 0.5, 0.5)),
-        transform: Transform::from_translation(Vec3::new(0.0, -50.0, 1.0))
-            .with_scale(Vec2::splat(30.).extend(1.)),
-        ..default()
-    });
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Circle::default()).into(),
+            material: materials.add(Color::rgb(1.0, 0.5, 0.5)),
+            transform: Transform::from_translation(Vec3::new(0.0, -50.0, 1.0))
+                .with_scale(Vec2::splat(30.).extend(1.)),
+            ..default()
+        },
+        Puyo,
+    ));
+}
+
+// ぷよを落下させる
+fn fall_puyo(mut query: Query<&mut Transform, With<Puyo>>) {
+    for mut transform in query.iter_mut() {
+        transform.translation.y -= 1.0;
+    }
 }
